9ee882896d7bdff3b802c44ecdd68eb646ef9b5a
Anghelo Alagon <200518@unsaac.edu.pe>
2024-09-09 11:47:04 -0500

    add Leftline component to Teams section

--- src/app/components/AcmTeams/index.jsx
+++ src/app/components/AcmTeams/index.jsx
@@ -1,4 +1,5 @@
 import H2 from "../common/H2"
+import LeftLine from "../common/LeftLine"
 import { BrainSvg } from "../Svgs/BrainSvg"
 import { CupSvg } from "../Svgs/CupSvg"
 import { PcSvg } from "../Svgs/PcSvg"
@@ -46,18 +47,22 @@ const Teams = [
 
 const AcmTeams = () => {
   return (
-    <section className="h-auto md:h-[90vh] w-auto flex flex-col items-center justify-around py-20 md:mx-28 text-white-azul">
+    <section className="h-auto md:h-[90vh] w-auto flex flex-col items-center justify-around py-20  text-white-azul">
       <H2 title="teams" />
-      <div className="w-full flex flex-wrap flex-col md:flex-row justify-between items-center gap-x-2 gap-y-6">
-        {Teams.map((team, index) => {
-          return (
-            <ArticlesTeams key={index} color={team.color} content={team.content} links={team.links}>
-              {team.svg}
-            </ArticlesTeams>
-          )
-        })
-        }
+      <div className="flex items-center justify-between w-full">
+        <LeftLine color="0A84FF" className="hidden md:flex" />
+        <div className="w-full flex flex-wrap flex-col md:flex-row md:px-10 justify-between items-center gap-x-2 gap-y-6 ">
+          {Teams.map((team, index) => {
+            return (
+              <ArticlesTeams key={index} color={team.color} content={team.content} links={team.links}>
+                {team.svg}
+              </ArticlesTeams>
+            )
+          })
+          }
+        </div>
       </div>
+
 
     </section>
   )
