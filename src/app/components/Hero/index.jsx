9aebe45a17bc8671d1879cb4ff28afdfbd7730ed
Anghelo Alagon <93554955+anghelo-code@users.noreply.github.com>
2024-09-09 21:42:45 -0500

    feat: create a component for the left line (#14)
    ### Changes
    
    - Create a component for the left line
    - It have to have a color property to paint the lines and the centered
    square
    - Replaced the left line image with the new LeftLine component 
    - Add LeftLine component to Teams section
    
    ---
    Close #8

--- src/app/components/Hero/index.jsx
+++ src/app/components/Hero/index.jsx
@@ -2,17 +2,14 @@ import React from "react";
 import HeroText from "./HeroText";
 import SocialMedia from "./SocialMedia";
 import Image from "next/image";
+import LeftLine from "../common/LeftLine";
 
 const Hero = () => {
   return (
     <div className="flex justify-between items-center my-10">
       {/* Redes sociales */}
-      <Image
-        src="/lateral-vertical-line.svg"
-        alt="Line"
-        width={140}
-        height={450}
-      />
+      <LeftLine color='0A84FF' />
+
       {/* Parte principal */}
       <div className="flex justify-center items-center">
         <HeroText />
