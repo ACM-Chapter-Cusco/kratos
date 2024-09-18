675162c5a2a9ae877c7347f6cdf682ef71e01f90
STYersonH <102554781+STYersonH@users.noreply.github.com>
2024-09-17 20:49:35 -0500

    refactor: Refactoring to break global style file in component style file (#18)
    ### Changes
    
    From globals.css in the root i moved the respective styles for
    - component "AcmTeams" which had the styles to glow the shadow for their
    cards
    - component "Hero" which had the styles to glow the main logo
    - component "Logo" in "Navigation" which had the styles to animate and
    shape the logo
    
    ---
    
    Close #9

--- src/app/components/Navigation/Logo.jsx
+++ src/app/components/Navigation/Logo.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import styles from "./Logo.module.css";
 
 const Logo = () => {
   return (
-    <div className="logo-container ml-8">
-      <div className="rombo"></div>
-      <div className="circle"></div>
+    <div className={`${styles.logo_container} ml-8`}>
+      <div className={styles.rombo}></div>
+      <div className={styles.circle}></div>
     </div>
   );
 };
