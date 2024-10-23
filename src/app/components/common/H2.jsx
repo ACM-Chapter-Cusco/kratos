bc19f003a741758450cc81307ab15801cb7a1a42
Anghelo Alagon <200518@unsaac.edu.pe>
2024-10-23 12:05:50 -0500

    fix responsive h2

--- src/app/components/common/H2.jsx
+++ src/app/components/common/H2.jsx
@@ -2,7 +2,7 @@ const H2 = ({ title }) => {
   const textStart = `<`;
   const textEnd = '/>';
   return (
-    <h2 className="font-extrabold text-7xl" >{textStart} <span className="text-blue">ACM</span>.{title} {textEnd}</h2>
+    <h2 className="font-extrabold text-5xl mb-8 lg:mb-0 md:text-7xl " >{textStart} <span className="text-blue">ACM</span>.{title} {textEnd}</h2>
   )
 }
 
