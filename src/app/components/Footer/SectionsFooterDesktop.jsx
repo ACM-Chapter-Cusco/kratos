3ddb67e515f89e7e5a8557cbb5034ccd6c18b7eb
STYersonH <102554781+STYersonH@users.noreply.github.com>
2024-10-24 10:56:56 -0500

    feat: Make the footer responsive
    Have been created anothe component called SectionsFooterMobile and has
    been changed the original SectionsFooter to SectionsFooterDesktop and
    it's been modified with tailwindcss utilities to make the footer
    responsive
    
    Close #38

--- src/app/components/Footer/SectionsFooterDesktop.jsx
+++ src/app/components/Footer/SectionsFooterDesktop.jsx
@@ -1,13 +1,13 @@
-import React from 'react'
-import LinksFooter from './LinksFooter'
-import TitlesFooter from './TitlesFooter'
+import React from "react";
+import LinksFooter from "./LinksFooter";
+import TitlesFooter from "./TitlesFooter";
 
-export const SectionsFooter = () => {
+export const SectionsFooterDesktop = () => {
   return (
-    <div className="flex flex-col flex-wrap md:flex-row justify-between items-center gap-x-40 gap-y-6">
+    <div className="flex items-center justify-between md:flex-row">
       <div>
         <TitlesFooter title="About" />
-        <div className="flex flex-col justify-start">
+        <div className="mt-8 flex flex-col justify-start">
           <LinksFooter link="#">What is ACM?</LinksFooter>
           <LinksFooter link="#">Visión</LinksFooter>
           <LinksFooter link="#">Misión</LinksFooter>
@@ -16,24 +16,22 @@ export const SectionsFooter = () => {
       </div>
       <div>
         <TitlesFooter title="Events" />
-        <div className="flex flex-col">
+        <div className="mt-8 flex flex-col">
           <LinksFooter link="#">Cuscontest</LinksFooter>
           <LinksFooter link="#">Todos vuelven</LinksFooter>
           <LinksFooter link="#">Robótica</LinksFooter>
           <LinksFooter link="#">Icpc</LinksFooter>
         </div>
-
       </div>
       <div>
         <TitlesFooter title="Teams" />
-        <div className="flex flex-col">
+        <div className="mt-8 flex flex-col">
           <LinksFooter link="#">development</LinksFooter>
           <LinksFooter link="#">investigation</LinksFooter>
           <LinksFooter link="#">programming</LinksFooter>
           <LinksFooter link="#">members</LinksFooter>
         </div>
-
       </div>
     </div>
-  )
-}
+  );
+};
--- src/app/components/Footer/index.jsx
+++ src/app/components/Footer/index.jsx
@@ -1,21 +1,28 @@
-import React from 'react'
-import { SectionsFooter } from './SectionsFooter'
-import Image from 'next/image'
-import SocialMediaFooter from './SocialMediaFooter'
-import LinksFooter from './LinksFooter'
+import React from "react";
+import { SectionsFooter, SectionsFooterDesktop } from "./SectionsFooterDesktop";
+import Image from "next/image";
+import SocialMediaFooter from "./SocialMediaFooter";
+import LinksFooter from "./LinksFooter";
+import { SectionsFooterMobile } from "./SectionsFooterMobile";
 
 const Footer = () => {
   return (
     <footer className="flex justify-center bg-dark-1">
-      <div className="h-auto w-[1400px] px-4 flex flex-col py-8 px-12 text-base text-white-azul gap-y-6" >
-        <div className="flex flex-col md:flex-row justify-between items-center mb-4 gap-y-6 gap-x-4">
-          <div className="flex flex-col items-center gap-y-4">
+      <div className="flex h-auto w-[80%] flex-col gap-y-6 px-12 px-4 py-8 text-base text-white-azul md:w-[1400px]">
+        <div className="mb-4 flex flex-col items-start justify-between gap-x-4 gap-y-6 md:flex-row md:items-center">
+          <div className="flex flex-col items-start gap-y-10 md:gap-y-4">
             <h3 className="text-base font-bold">ACM.Chapter.Cusco</h3>
             <SocialMediaFooter />
           </div>
-          <SectionsFooter />
 
-          <div className="flex">
+          <div className="hidden w-[50%] md:block">
+            <SectionsFooterDesktop />
+          </div>
+          <div className="my-5 w-full md:my-0 md:hidden">
+            <SectionsFooterMobile />
+          </div>
+
+          <div className="flex w-full justify-center md:w-auto">
             <Image
               src="/info_logo.png"
               alt="informatica logo"
@@ -31,18 +38,15 @@ const Footer = () => {
               className=""
             />
           </div>
-
         </div>
-        <div className="flex flex-row justify-center items-center gap-6">
+        <div className="flex flex-row items-center justify-center gap-6">
           <LinksFooter link="#">Help</LinksFooter>
           <LinksFooter link="#">Terminos</LinksFooter>
           <LinksFooter link="#">Estatuto</LinksFooter>
         </div>
       </div>
-
-
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
