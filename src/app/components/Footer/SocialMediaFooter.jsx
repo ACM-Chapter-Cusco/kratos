69082b34f1c1156d63d478b8a5cc6f64598b345b
STYersonH <102554781+STYersonH@users.noreply.github.com>
2024-10-29 11:52:57 -0500

    feat: Make the footer responsive (#39)
    ### Changes
    
    Have been created anothe component called SectionsFooterMobile and has
    been changed the original SectionsFooter to SectionsFooterDesktop and
    it's been modified with tailwindcss utilities to make the footer
    responsive
    
    ---
    
    Close #38

--- src/app/components/Footer/SocialMediaFooter.jsx
+++ src/app/components/Footer/SocialMediaFooter.jsx
@@ -8,39 +8,39 @@ import { MdEmail } from "react-icons/md";
 
 const SocialMediaFooter = () => {
   return (
-    <div className="flex gap-x-3">
+    <div className="flex gap-x-8 md:gap-x-3">
       <a href="#">
         <FaFacebookSquare
-          className="text-white-azul hover:text-blue transition duration-300 "
+          className="text-white-azul transition duration-300 hover:text-blue"
           size="21px"
         />
       </a>
       <a href="#">
         <FaInstagram
-          className="text-white-azul hover:text-rojo transition duration-300"
+          className="text-white-azul transition duration-300 hover:text-rojo"
           size="21px"
         />
       </a>
       <a href="#">
         <FaGithubAlt
-          className="text-white-azul hover:text-verde transition duration-300"
+          className="text-white-azul transition duration-300 hover:text-verde"
           size="21px"
         />
       </a>
       <a href="#">
         <FaDiscord
-          className="text-white-azul hover:text-cyan transition duration-300"
+          className="text-white-azul transition duration-300 hover:text-cyan"
           size="21px"
         />
       </a>
       <a href="#">
         <MdEmail
-          className="text-white-azul hover:text-rojo transition duration-300"
+          className="text-white-azul transition duration-300 hover:text-rojo"
           size="21px"
         />
       </a>
     </div>
-  )
-}
+  );
+};
 
-export default SocialMediaFooter
+export default SocialMediaFooter;
