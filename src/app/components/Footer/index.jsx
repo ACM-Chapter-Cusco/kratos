e62e91da011b90bcb8c595c9f895e68d8cc83a8c
STYersonH <102554781+STYersonH@users.noreply.github.com>
2024-11-13 15:19:13 -0500

    adding links to social media

--- src/app/components/Footer/index.jsx
+++ src/app/components/Footer/index.jsx
@@ -8,9 +8,9 @@ import { SectionsFooterMobile } from "./SectionsFooterMobile";
 const Footer = () => {
   return (
     <footer className="flex justify-center bg-dark-1">
-      <div className="flex h-auto w-[80%] flex-col gap-y-6 px-12 px-4 py-8 text-base text-white-azul md:w-[1400px]">
-        <div className="mb-4 flex flex-col items-start justify-between gap-x-4 gap-y-6 md:flex-row md:items-center">
-          <div className="flex flex-col items-start gap-y-10 md:gap-y-4">
+      <div className="flex h-auto w-[80%] flex-col gap-y-6 px-4 py-8 text-base text-white-azul md:w-[1400px] md:px-12">
+        <div className="mb-4 flex flex-col items-center justify-between gap-x-4 gap-y-6 md:flex-row md:items-center">
+          <div className="flex flex-col items-center gap-y-10 md:gap-y-4">
             <h3 className="text-base font-bold">ACM.Chapter.Cusco</h3>
             <SocialMediaFooter />
           </div>
