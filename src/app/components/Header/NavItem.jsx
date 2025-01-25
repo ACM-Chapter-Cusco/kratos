2d97c7d32b98da11439393d3b21582c301b3c244
STYersonH <102554781+STYersonH@users.noreply.github.com>
2025-01-25 04:06:54 -0500

    feat: Develop the simulator of code (#51)
    ### Changes
    
    Has been developed according the design the simulator of code responsive
    
    ---
    
    Close #42

--- src/app/components/Header/NavItem.jsx
+++ src/app/components/Header/NavItem.jsx
@@ -2,13 +2,13 @@ import React, { useState } from "react";
 import { FaChevronDown, FaChevronUp } from "react-icons/fa";
 import Subitems from "./Subitems";
 
-const NavItem = ({ valor, subItems }) => {
+const NavItem = ({ href, valor, subItems }) => {
   const [itemOpen, setItemOpen] = useState(false);
 
   return (
     <div className="relative">
       <a
-        href="#"
+        href={href}
         className="flex items-center justify-center gap-3 border-b-2 border-azul-git border-opacity-0 font-Opensans text-3xl text-white-azul transition duration-300 hover:border-blue hover:border-opacity-100 md:gap-1 md:text-xl"
         onClick={() => setItemOpen(!itemOpen)}
       >
--- src/app/cuscontest/page.jsx
+++ src/app/cuscontest/page.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import Header from "../components/Header";
 import Hero from "./components/Hero";
+import SimulatorSection from "./components/SimulatorSection";
+import About from "./components/About";
 
 const CuscontestPage = () => {
   return (
@@ -10,6 +12,8 @@ const CuscontestPage = () => {
         <div className="flex w-full justify-center">
           <div className="w-pantalla lg mt-28 flex h-auto flex-col items-center gap-10">
             <Hero />
+            <SimulatorSection />
+            <About />
           </div>
         </div>
       </div>
